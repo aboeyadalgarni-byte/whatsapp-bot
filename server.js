7f793ea671c297a57c86e7c4c71ba4b0df826f0e
aboeyadalgarni-byte <aboeyad.algarni@gmail.com>
2026-05-27 01:01:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8815356896:AAFzhaQIV9JRlVOIIPJGnCfQNqsIcE3Qn7U'
+const token = '8815356896:AAFzhaQIV9JRlVOIIPJGnCfQNqsIcE3Qn7U';
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -10,13 +10,13 @@ bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, '🔥 بوت حراج شغال');
 });
 
-// رسالة تجريبية كل دقيقة
+// إرسال فرصة كل دقيقة
 setInterval(() => {
 
   bot.sendMessage(channel, `
 🚨 فرصة جديدة من حراج
 
-كامري 2024 فل كامل
+🚗 كامري 2024 فل كامل
 
 💰 السعر: 89,000 ريال
 
